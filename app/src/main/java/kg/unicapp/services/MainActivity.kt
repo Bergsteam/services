fe9855db4de8fb6de3b4c232ffdcec8cf383df58
Sabira <mnsabira@gmail.com>
2021-09-20 12:13:40 +0400

    changes were deleted

--- app/src/main/java/kg/unicapp/services/MainActivity.kt
+++ app/src/main/java/kg/unicapp/services/MainActivity.kt
@@ -2,7 +2,6 @@ package kg.unicapp.services
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kg.unicapp.services.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
