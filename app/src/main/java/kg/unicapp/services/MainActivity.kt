a6cbad0dc8921710bed633ab68d954605fd983c7
Sabira <mnsabira@gmail.com>
2021-09-21 20:06:35 +0400

    dependencies of fireBase was added and dataBase Real time was tested

--- app/src/main/java/kg/unicapp/services/MainActivity.kt
+++ app/src/main/java/kg/unicapp/services/MainActivity.kt
@@ -1,11 +1,15 @@
 package kg.unicapp.services
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
